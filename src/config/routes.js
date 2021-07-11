f0e3ef76c92a68f7cb8a0e29e8429c2432db4a45
Lucas <lucaspoletis@gmail.com>
2021-07-11 19:43:25 -0300

    tratando erros com inserir conta e refactoring

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
   app.route('/users')
-  .get(app.routes.users.findAll)
-  .post(app.routes.users.create);
+    .get(app.routes.users.findAll)
+    .post(app.routes.users.create);
+
+  app.route('/accounts')
+    .post(app.routes.accounts.create);
 };
---
+++ src/services/account.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+    const save = (account) => {
+        return app.db('accounts').insert(account, '*');
+    };
+
+    return {
+        save
+    };
+};

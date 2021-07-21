73279447b4318618ac4a1974d94f72ce63cd96e2
Lucas <lucaspoletis@gmail.com>
2021-07-21 19:00:29 -0300

    adicionando verificação de transferencias

--- test/routes/transfers.test.js
+++ test/routes/transfers.test.js
@@ -163,3 +163,59 @@ test('Deve retornar uma transferencia por Id', () => {
             expect(res.body.description).toBe('Transfer #1');
         });
 });
+
+describe('Ao alterar uma transferência valida ...', () => {
+    let transferId;
+    let income;
+    let outcome;
+
+    test('Deve retornar o status 200 e os dados da transferência', () => {
+        return request(app).put(`${MAIN_ROUTE}/10000`)
+            .set('authorization', `bearer ${TOKEN}`)
+            .send({
+                description: 'Transfer Updated',
+                user_id: 10000,
+                acc_ori_id: 10000,
+                acc_dest_id: 10001,
+                ammount: 500,
+                date: new Date()
+            })
+            .then(async (res) => {
+                expect(res.status).toBe(200);
+                expect(res.body.description).toBe('Transfer Updated');
+                expect(res.body.ammount).toBe('500.00');
+                transferId = res.body.id;
+            });
+    });
+    test('As transações equivalentes devem ter sido geradas', async () => {
+        const transactions = await app.db('transactions').where({
+            transfer_id: transferId
+        }).orderBy('ammount');
+        expect(transactions).toHaveLength(2);
+        [outcome, income] = transactions;
+    });
+
+    test('A transação de saida deve ser negativa', () => {
+        expect(outcome.description).toBe('Transfer to acc #10001');
+        expect(outcome.ammount).toBe('-500.00');
+        expect(outcome.acc_id).toBe(10000);
+        expect(outcome.type).toBe('O');
+
+    });
+
+    test('A transação de saida deve ser positiva', () => {
+        expect(income.description).toBe('Transfer from acc #10000');
+        expect(income.ammount).toBe('500.00');
+        expect(income.acc_id).toBe(10001);
+        expect(income.type).toBe('I');
+
+    });
+
+    test('Ambas devem referenciar a transferencia que as originou', () => {
+        expect(income.transfer_id).toBe(transferId);
+        expect(outcome.transfer_id).toBe(transferId);
+
+    });
+
+
+});
